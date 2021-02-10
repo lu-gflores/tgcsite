9e3f709640ecadb736cb9d339da30ac528d52752
George Flores <georgeflores886@gmail.com>
2021-02-10 16:41:39 -0500

    updated css format for cards

--- src/components/PokemonCardList.js
+++ src/components/PokemonCardList.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
 import {Link} from 'react-router-dom'
 import {Container, Pagination} from 'semantic-ui-react'
 
@@ -42,28 +43,39 @@ const PokemonCardList = () => {
     return (
         <Container>
         
-        <div className="ui center aligned action input">
+        <SearchInput className="ui action input">
           <input type="text" onChange={queryCard} name='card' placeholder='Search card...'/>
           <button className='ui button' onClick={searchCard} type='button'>Search</button>
-        </div>
+        </SearchInput>
 
         {pokemon && (
 
-        <div className="ui four cards">
-
+        <div className="ui centered cards">
+        
           {pokemon.map(pokeCard => (       
-              <Link to={`/${pokeCard.id}`} name={pokeCard.name} image={pokeCard.images.small} className="ui raised card" key={pokeCard.id}>
+              <Link to={`/${pokeCard.id}`} name={pokeCard.name} image={pokeCard.images.small} className="ui card" key={pokeCard.id}>
                 <div className="image">
                   <img src={`${pokeCard.images.small}`} alt={`${pokeCard.name}`} />
                 </div>
               </Link>    
           ))}
         </div> 
-        )}        
-          <Pagination activePage={activePage} onPageChange={onChange} totalPages={10} ellipsisItem={null} />
+        )}  
+          
+          <Container textAlign='center' >
+            <Pagination activePage={activePage} onPageChange={onChange} totalPages={10} ellipsisItem={null} />
+          </Container>
+        
         </Container>
     )
 }
+
+const SearchInput = styled.div.attrs(props => ({
+  className: 'ui action input',
+}))`
+  padding: 1.5rem 1.5rem;
+`
+
 
 
 export default PokemonCardList

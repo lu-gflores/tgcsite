93a08777d1558565fdd5c99cba05fbfcc34e6e04
George Flores <georgeflores886@gmail.com>
2021-02-15 16:51:49 -0500

    updated CardDetails page to use hooks

--- src/pages/CardDetails.js
+++ src/pages/CardDetails.js
@@ -1,44 +1,77 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import {Container, Segment, Image} from 'semantic-ui-react'
+import Pokemon from '../components/Pokemon'
 
-class CardDetails extends React.Component {
-    constructor() {
-        super()
-        this.state = {data: []}
-    }
-    componentDidMount () {
-        axios.get(`https://api.pokemontcg.io/v2/cards/${this.props.match.params.id}`)
-        .then(card=>{
-            console.log(card.data.data)
-            this.setState({data: card.data.data})
-        }).catch(err => console.log(err))        
-    }
-    render() {
-        // const attackMoves = this.state.data.attacks.map(attack => {
-        //     console.log(attack)
-            // return (<li key={attack}>{attack}</li>)
-        // })
-        return(
-            <Container>
-                <Link to='/' className='ui button'>Back</Link>
-                {this.state.data.length !==0 &&
-                <Segment> 
-                    <Image src={this.state.data.images.small} alt={this.state.data.name} floated='left'/>
-                    <h2>{this.state.data.name} Lv. {this.state.data.level}</h2>
-                    <h5>Artist: {this.state.data.artist}</h5>
-                    {/* <p><strong>{this.state.data.attacks[0].name}</strong> : {this.state.data.attacks[1].text} </p>
-                    <p><strong>{this.state.data.attacks[1].name === null}</strong> : {this.state.data.attacks[1].text} </p> */}
+const CardDetails = () => {
+    const [card, setCard] = useState([])
+    const {id} = useParams()
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await
+            axios.get(`https://api.pokemontcg.io/v2/cards/${id}`)
+            console.log(result.data.data)
+            setCard(result.data.data)  
+        }
+        fetchData()      
+    }, [id])
 
-                </Segment>
-                }
-            </Container>
-        )
-    }
+    return(
+        <Container>
+            <Link to='/' className='ui button'>Back</Link>
 
+            {card.length !== 0  && 
+                
+            <Segment>
+                <Image src={card.images.small}  alt={card.name} floated='left'/>
+                    <h2>{card.name}</h2>
+                    <h5>Rarity: {card.rarity}</h5>
+                    <h5>Artist: {card.artist}</h5>
+            </Segment>      
+             }
+        </Container>
+    )
 }
+
+// class CardDetails extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {data: []}
+//     }
+//     componentDidMount () {
+//         axios.get(`https://api.pokemontcg.io/v2/cards/${this.props.match.params.id}`)
+//         .then(card=>{
+//             console.log(card.data.data)
+//             this.setState({data: card.data.data})
+//         }).catch(err => console.log(err))        
+//     }
+//     render() {
+//        const cardType = this.state.data.supertype 
+//        if(cardType === 'Pokemon') {
+//         <Pokemon name={this.state.data.name} />
+//        }
+//         return(
+//             <Container>
+//                 <Link to='/' className='ui button'>Back</Link>
+//                 {this.state.data.length !==0 &&
+//                 <Segment> 
+//                     <Image src={this.state.data.images.small} alt={this.state.data.name} floated='left'/>
+//                     <h2>{this.state.data.name} </h2>
+//                     <h5>Artist: {this.state.data.artist}</h5>
+                    
+//                     {/* <p><strong>{this.state.data.attacks[0].name}</strong> : {this.state.data.attacks[1].text} </p>
+//                     <p><strong>{this.state.data.attacks[1].name === null}</strong> : {this.state.data.attacks[1].text} </p> */}
+
+
+//                 </Segment>
+//                 }
+//             </Container>
+//         )
+//     }
+
+// }
 
 
 
